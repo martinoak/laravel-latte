25a105177c3f2c7e0c4c48243cfdb26d2b22aae1
Martin Dub <martin.dub@dek-cz.com>
2023-06-02 08:41:19 +0200

    Remove + change

--- src/LaravelLatte/ServiceProvider.php
+++ src/LaravelLatte/ServiceProvider.php
@@ -33,8 +33,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $latte = new Latte();
         $latte->setTempDirectory($config->get('view.compiled'));
 
-        $latte->addFunction('link', [LatteFunctions::class, 'link']); // @deprecated
-
         $latte->addExtension(new Extension());
 
         return $latte;
